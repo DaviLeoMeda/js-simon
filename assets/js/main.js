6e361e286c84bc76d3625d1fd9e905be8b67bd42
DaviLeoMeda <davileo.meda@gmail.com>
2023-04-04 17:27:21 +0200

    First Step

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,15 @@
 function numeriPc(max, min) {
     Math.floor(Math.random() * max) + min;
 }
+
+let numCpu = numeriPc(5, 1);
+let numberPlace = document.getElementById("numberHere");
+
+array = [];
+
+start.addEventListener("click", () {
+
+    let numbSequen = setInterval(function () )
+
+    numbersHere.innerHTML = 
+})
